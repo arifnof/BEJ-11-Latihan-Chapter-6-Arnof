c76cc07ab7f0897eeefdbef399774f141146168f
arifnofyansyah <arifnofyansyah@gmail.com>
2024-06-08 07:13:41 +0700

    Modify user and post migration and seeder

---
+++ be/src/infrastructure/database/migrations/20240608000806-create-post.js
@@ -0,0 +1,64 @@
+"use strict"
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("posts", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+      },
+      description: {
+        type: Sequelize.TEXT,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn("NOW"),
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn("NOW"),
+      },
+    })
+    await queryInterface.createTable("media", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      post_id: {
+        type: Sequelize.INTEGER,
+      },
+      media_url: {
+        type: Sequelize.STRING,
+      },
+      media_type: {
+        type: Sequelize.STRING(10),
+      },
+      caption: {
+        type: Sequelize.TEXT,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn("NOW"),
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.fn("NOW"),
+      },
+    })
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("posts")
+    await queryInterface.dropTable("media")
+  },
+}
